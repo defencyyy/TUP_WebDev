736d28139fedfdf06b66f02ad61d2e141fad2f1a
Manga <cyrusparaiso@gmail.com>
2023-09-24 23:06:58 +0800

    Fixing projects section

--- script.js
+++ script.js
@@ -23,10 +23,8 @@ hiddenElements3.forEach((el) => observer.observe(el));
 const hiddenElements4 = document.querySelectorAll('.hidden-1');
 hiddenElements4.forEach((el) => observer.observe(el));  
 
-
-
-
-const wrapper = document.querySelector(".wrapper_project");
+/* Projects */
+const wrapper_project = document.querySelector(".wrapper_project");
 const carousel = document.querySelector(".carousel");
 const firstCardWidth = carousel.querySelector(".card").offsetWidth;
 const arrowBtns = document.querySelectorAll(".wrapper_project i");
@@ -34,14 +32,6 @@ const carouselChildrens = [...carousel.children];
 let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
 // Get the number of cards that can fit in the carousel at once
 let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
-// Insert copies of the last few cards to beginning of carousel for infinite scrolling
-carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
-    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
-});
-// Insert copies of the first few cards to end of carousel for infinite scrolling
-carouselChildrens.slice(0, cardPerView).forEach(card => {
-    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
-});
 // Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
 carousel.classList.add("no-transition");
 carousel.scrollLeft = carousel.offsetWidth;
@@ -52,19 +42,3 @@ arrowBtns.forEach(btn => {
         carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
     });
 });
-const dragStart = (e) => {
-    isDragging = true;
-    carousel.classList.add("dragging");
-    // Records the initial cursor and scroll position of the carousel
-    startX = e.pageX;
-    startScrollLeft = carousel.scrollLeft;
-}
-const dragging = (e) => {
-    if(!isDragging) return; // if isDragging is false return from here
-    // Updates the scroll position of the carousel based on the cursor movement
-    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
-}
-const dragStop = () => {
-    isDragging = false;
-    carousel.classList.remove("dragging");
-}
