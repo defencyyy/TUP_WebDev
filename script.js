6cbb5b572a9195045515ccca5416a02ab393abab
Manga <cyrusparaiso@gmail.com>
2023-09-25 00:07:19 +0800

    Need nalang ng actual projects

--- script.js
+++ script.js
@@ -23,22 +23,40 @@ hiddenElements3.forEach((el) => observer.observe(el));
 const hiddenElements4 = document.querySelectorAll('.hidden-1');
 hiddenElements4.forEach((el) => observer.observe(el));  
 
-/* Projects */
+/* Projects*/
 const wrapper_project = document.querySelector(".wrapper_project");
 const carousel = document.querySelector(".carousel");
 const firstCardWidth = carousel.querySelector(".card").offsetWidth;
 const arrowBtns = document.querySelectorAll(".wrapper_project i");
 const carouselChildrens = [...carousel.children];
+
 let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
-// Get the number of cards that can fit in the carousel at once
+
 let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
-// Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
-carousel.classList.add("no-transition");
-carousel.scrollLeft = carousel.offsetWidth;
-carousel.classList.remove("no-transition");
-// Add event listeners for the arrow buttons to scroll the carousel left and right
+
 arrowBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
     });
 });
+
+const dragStart = (e) => {
+    isDragging = true;
+    carousel.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+    if(!isDragging) return; 
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () => {
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
