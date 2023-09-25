65d2de216180f2e6a4d558fd205d38c26712a4f5
Manga <cyrusparaiso@gmail.com>
2023-09-26 00:13:03 +0800

    Finished nav
    kailangan nalang ng responsiveness and ung pagpindot ng nav links

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+/* Scroll */
+const nav = document.querySelector("nav");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 60) {
+    nav.classList.add("scrolled");
+  }
+  else {
+    nav.classList.remove("scrolled");
+  }
+})
+
 /* Fade Effects */
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
