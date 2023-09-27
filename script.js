a04fbdde0cb1048525320dac165d28bf4c38f05f
Manga <cyrusparaiso@gmail.com>
2023-09-28 01:46:21 +0800

    FINISHED MEDIA FOR MOBILE

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 /* Scroll */
 const nav = document.querySelector("nav");
+const mobileNav = document.querySelector("nav.mobile-nav")
+const menuIcon = document.querySelector(".menu-icon")
+const closeIcon = document.querySelector(".mobile-menu .close-icon")
+const mobileMenu = document.querySelector(".mobile-menu")
 
 window.addEventListener("scroll", () => {
   if (window.scrollY > 60) {
     nav.classList.add("scrolled");
+    mobileNav.classList.add("scrolled");
   }
   else {
     nav.classList.remove("scrolled");
+    mobileNav.classList.remove("scrolled");
   }
-})
+});
+
+menuIcon.addEventListener("click", () => {
+  mobileMenu.classList.add("active");
+});
+
+closeIcon.addEventListener("click", () => {
+  mobileMenu.classList.remove("active");
+});
 
 /* Fade Effects */
 const observer = new IntersectionObserver((entries) => {
